9eebc1ee7bc7b177413c4bff972fd7d029dcde7a
Aaron Rose <arose@ballytech.com>
2014-11-10 14:05:09 -0800

    Got just a moving box working.

--- js/canvas.js
+++ js/canvas.js
@@ -1,5 +1,13 @@
 var CANVAS_WIDTH = 480;
 var CANVAS_HEIGHT = 320;
 var canvasElement = $("<canvas width='" + CANVAS_WIDTH + "' height='" + CANVAS_HEIGHT + "'></canvas");
-var canvas = canvasElement.get(0).getContext("2d");
-canvasElement.appendTo('body');
+var canvas;
+
+function InitCanvas() {
+    canvas = canvasElement.get(0).getContext("2d");
+    canvasElement.appendTo('body');
+}
+
+function ClearCanvas() {
+    canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
